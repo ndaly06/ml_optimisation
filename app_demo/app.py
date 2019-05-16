9ac219c30fad32c5e8417b484084410123c1daa3
Nial Daly <ndaly06@qub.ac.uk>
2019-05-16 01:07:02 +0100

    custom favicon support added

--- app_demo/app.py
+++ app_demo/app.py
@@ -1,68 +1,30 @@
 # # -*- coding: utf-8 -*-
-# import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
-
-# # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# app.title = 'QnA Demo'
-
-# # Configure navbar menu
-# nav_menu = html.Div([
-#     html.Nav([
-
-#             ], className='navbar navbar-default', style={'backgroundColor': 'orange', 'height': '40px', 'margin': '0', 'border': '0'} ),
-# ], className='navbar navbar-default', style={'backgroundColor': 'orange', 'height': '40px', 'margin': '0', 'border': '0'})
-
-# #
-# app.layout = html.Div(
-#     children=[
-#         nav_menu,
-
-    
-
-#     # html.Nav(
-#     #     className = "navbar-default navbar-static-top",
-#     #     style={'backgroundColor': '#fa4f56', 'height': '60px', 'padding': '0px'},
-
-#     #     children=[
-#     #     html.A('Demo', className="brand-logo")]
-#     #     ),
-
-#     # html.Div(
-#     #     id='top-bar',
-#     #     className='row',
-#     #     style={'backgroundColor': '#fa4f56',
-#     #     'height': '5px',
-#     #     }
-#     # ),
-# ])
-
-# # Add bootstrap css
-# app.css.append_css({"external_url": [
-#     "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-# ]})
-
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
-
-# 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import flask
+import os
 
+#
 app = dash.Dash(__name__)
 server = app.server
 app.title = 'optML'
 
-#
+# replaces the dash favicon with custom favicon in static
+@server.route('/favicon.ico')
+def favicon():
+    return flask.send_from_directory(os.path.join(server.root_path, 'static'),
+                                     'favicon.ico')
+
+# app layout
 app.layout = html.Div(children=[
     
     html.Nav(children=[
-        html.H4('optML',
-        style={'text-align': 'vertical-align', 'display': 'inline-block', 'color': 'white'}
+        html.Div(
+            html.H4(
+                'optML'
+            ),
+        className = "container", style={'width': '10%', 'text-align': 'vertical-align', 'display': 'inline-block', 'color': 'white'}
         )
         ], 
         className = "nav nav-pills", style={'backgroundColor': '#D50000', 'width': '100%', 'height': '45px'}
@@ -86,55 +48,36 @@ app.layout = html.Div(children=[
 
         html.Div(
 
-            html.Button('Ask Question', id='button'),
+            # html.Button('Ask Question', id='button'),
 
-            className = 'col-sm-5', style={'height': '400px', 'border': 'thin solid white', 'backgroundColor': '#212529', 'padding': '8px'}
+            className = 'col-sm-5', style={'height': '300px', 'border': 'thin solid white', 'backgroundColor': '#212529', 'padding': '8px'}
+        ),
+
+        html.Div(
+
+            className = 'col-sm-10', style={'height': '300px', 'border': 'thin solid white', 'backgroundColor': '#212529', 'padding': '8px'}
+        ),
+
+        html.Div(
+
+            className = 'col-sm-2', style={'height': '300px', 'border': 'thin solid white', 'backgroundColor': '#212529', 'padding': '8px'}
+        ),
+
+        html.Div(
+
+            className = 'col-sm-12', style={'height': '300px', 'border': 'thin solid white', 'backgroundColor': '#212529', 'padding': '8px'}
         ),
 
     ],
     className = 'container', style={'width': '100%', 'height': '1000px', 'border': 'thin solid black', 'backgroundColor': '#212529'}
     )
 
-
-
-
-],
-
-
-
-    # nav_menu,
-    # children=[
-    #     # navbar
-    #     html.Nav(
-    #         className = 'navbar navbar-default navbar-static-top', style={'backgroundColor': 'orange', 'width': '100%'}
-    #     ),
-
-    #     html.Div(
-    #         children=[
-    #             html.Label('Context', style={'color': '#8cabd9'}),
-
-    #         ],
-    #         className = 'container', style={'width': '100%', 'max-height': '100%', 'border': 'thin solid white'}
-    #     ),
-
-
-    #     html.Div(
-    #         children=[
-    #             html.Label('Context', style={'color': '#8cabd9'}),
-
-    #         ],
-    #         className = 'container', style={'width': '100%', 'max-height': '100%', 'border': 'thin solid white'}
-    #     ),
-
-
-    # ],
-    # className = 'container', style={'width': '100%', 'height': '100%', 'border': '0', 'backgroundColor': '#212529'}
-
-)
+])
 
 # Add bootstrap css
 app.css.append_css({"external_url": [
-    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+    "https://raw.githubusercontent.com/ndaly06/ml_optimisation/master/app_demo/assets/styles.css"
 ]})
 
 if __name__ == '__main__':
